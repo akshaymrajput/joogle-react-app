2531e0b0cad2ac6545264675541787c0ccc24621
ga7axy <akshaymrajput@gmail.com>
2024-07-23 16:19:26 +0530

    local file in public

--- src/contexts/ResultContextProvider.jsx
+++ src/contexts/ResultContextProvider.jsx
@@ -16,35 +16,47 @@ export const ResultContextProvider = ({ children }) => {
     const getResults = async (type) => {
         try {
             setIsLoading(true);
-            const response = await fetch(`${baseUrl}${type}`, {
+            const encodedSearchTerm = encodeURIComponent(searchTerm);
+            const url = `${baseUrl}${type}&q=${encodedSearchTerm}`;
+            console.log(`Fetching results from API: ${url}`);
+            const response = await fetch(url, {
                 method: "GET",
                 headers: {
                     "x-rapidapi-host": baseUrl,
                     "x-rapidapi-key": import.meta.env.VITE_REACT_APP_API_KEY,
                 },
             });
+
+            console.log(`API response status: ${response.status}`);
+
             if (response?.status !== 200) {
+                console.log(
+                    "API limit reached or other error occurred. Fetching dummy data."
+                );
                 const dataResponse = await fetch("/data.json");
                 const imagesResponse = await fetch("/images.json");
+
                 const data = await dataResponse.json();
                 const images = await imagesResponse.json();
+
                 setDummyData(data.items);
                 setDummyImages(images.items);
                 setError(true);
                 setIsLoading(false);
                 setApiLimitReached(true);
-                console.log("Monthly API Call Limit reached! :(");
                 console.log("Showing dummy data instead.");
             } else {
                 const data = await response.json();
+                console.log("API response data:", data);
                 setResults(data.items);
                 setIsLoading(false);
                 setError(false);
+                setApiLimitReached(false);
             }
         } catch (e) {
+            console.error("Error fetching results:", e.message);
             setError(true);
             setIsLoading(false);
-            console.log(e.message);
         }
     };
 
